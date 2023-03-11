a7361f77510dd8a08c34d628dace147e09b21349
antoshik26 <antoshik_26@mail.ru>
2023-03-11 16:45:44 +0300

    test_inVinci

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     '''
         В пременной token должен быть ваш токен сгенерированный в телеграмме BotFather
     '''
-    token = '5690057569:AAHNpVHJaylNch1Y7EAnWOq-s_pqfV-B1kk'
+    token = ''
     model = model_fastncc(0.5, 'models_labels/test_importance.csv')
     telegram = TelegramStart(token, model)
     telegram.start_bot()
